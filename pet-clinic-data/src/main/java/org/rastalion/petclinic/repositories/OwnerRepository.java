8b081faa0c9e79c7e5dc540177da9d794986b2e7
Mr Robot <alexander.keisse.opensource@google.com>
2021-01-05 14:45:46 +0100

    Implemented find owner.

--- pet-clinic-data/src/main/java/org/rastalion/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/org/rastalion/petclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package org.rastalion.petclinic.repositories;
 import org.rastalion.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/org/rastalion/petclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/org/rastalion/petclinic/services/map/OwnerMapService.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import javax.sound.midi.MidiFileFormat;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -77,5 +78,12 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
                 .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+
+        //TODO: impl
+        return null;
     }
 }
